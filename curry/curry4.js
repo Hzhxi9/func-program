6a525006f4681d292971a56341e967ae5dd568ea
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2021-11-29 08:53:01 +0800

    feat: curry placeholder

--- curry/curry4.js
+++ curry/curry4.js
@@ -1,45 +1,61 @@
-function curry(fn, placeholder = [], ...args) {
+function curry(fn, args = [], holes = []) {
   const length = fn.length;
-  return function (...args2) {
-    const _args = args.slice(),
-      _placeholder = placeholder.slice(),
-      argsLen = args.length,
-      placeholderLen = placeholder.length,
-      index = 0;
 
-    for (const arg of args2) {
-      /**
-       * 处理类似 fn(1, _, _, 4)(_, 3) 这种情况
-       * index 需要指向 holes 正确的下标
-       */
-      if (arg === _ && placeholderLen) {
+  return function () {
+    const _args = args.slice(),
+      _holes = holes.slice(),
+      argsLen = args.length,
+      holesLen = holes.length;
+      
+    let index = 0;
+
+    for (let i = 0, len = arguments.length; i < len; i++) {
+      const arg = arguments[i];
+      if (arg === _ && holesLen) {
+        /**
+         * 处理类似 fn(1, _, _, 4)(_, 3) 这种情况
+         * index 需要指向 holes 正确的下标
+         */
         index++;
-        if (index > placeholderLen) {
+        if (index > holesLen) {
           _args.push(arg);
-          _placeholder.push(argsLen - 1 + index - placeholderLen);
+          _holes.push(argsLen - 1 + index - holesLen);
         }
       } else if (arg === _) {
         /**处理类似 fn(1)(_) 这种情况 */
         _args.push(arg);
-        _placeholder.push(argsLen + 1);
-      } else if (placeholderLen) {
+        _holes.push(argsLen + i);
+      } else if (holesLen) {
         /**处理类似 fn(_, 2)(1) 这种情况 */
-        if (index >= placeholderLen) {
-          /**n(_, 2)(_, 3) */
+        if (index >= holesLen) {
+          /**fn(_, 2)(_, 3) */
           _args.push(arg);
         } else {
           /**fn(_, 2)(1) 用参数 1 替换占位符 */
-          _args.splice(_placeholder[index], 1, arg);
-          _placeholder.splice(index, 0);
+          _args.splice(_holes[index], 1, arg);
+          _holes.splice(index, 1);
         }
       } else {
         _args.push(arg);
       }
     }
-    if (_placeholder.length || _args.length < length) {
-      return curry.call(this, fn, _args, _placeholder);
+    if (_holes.length || _args.length < length) {
+      return curry.call(this, fn,  _args, _holes);
     } else {
       return fn.apply(this, _args);
     }
   };
 }
+
+let _ = {};
+
+let fn = curry(function (a, b, c, d, e) {
+  console.log([a, b, c, d, e]);
+});
+
+fn(1, 2, 3, 4, 5);
+fn(_, 1, 2, 3, 4)(5);
+fn(1, _, 3, 4, 5)(2)
+fn(1, _, 3)(_, 4)(2)(5);
+fn(1, _, _, 4)(_, 3)(2)(5);
+fn(_, 2)(_, _, 4)(1)(3)(5)
