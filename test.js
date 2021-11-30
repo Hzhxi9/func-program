c53db2f294ff8f91817829a60a5b6083be2eeb01
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2021-11-30 08:36:44 +0800

    feat: finish

--- test.js
+++ test.js
@@ -1,22 +1,39 @@
-const pipe =
-  (acc, cur) =>
-  (...args) =>
-    acc(cur(...args));
-
-const compose = (...funcs) => {
+const compose = function (...funcs) {
   const len = funcs.length;
   if (len === 0) return args => args;
   if (len === 1) return funcs[0];
   return funcs.reduce(pipe);
 };
 
+const pipe =
+  (acc, cur) =>
+  (...args) =>
+    acc(cur(...args));
+
 const init = (...args) => args.reduce((acc, cur) => acc + cur, 0);
+
 const fn1 = value => value + 1;
 const fn2 = value => value + 2;
 const fn3 = value => value + 3;
 
-const fns = [fn3, fn2, fn1, init]
+const fns = [fn3, fn2, fn1, init];
 
-const com = compose(...fns)
+const fnsCompose = compose(...fns);
 
-console.log(com(1, 2, 3))
+console.log(fnsCompose(1, 2, 3));
+
+const curry = (fn, ...args) => {
+  return function (...args2) {
+    const _args = [...args, ...args2];
+    if (fn.length > _args.length) return curry.call(this, fn, ..._args);
+    return fn.apply(this, _args);
+  };
+};
+
+const fn = (a, b, c) => a + b + c;
+
+const sum = curry(fn)
+
+console.log(sum(1)(2)(3))
+
+console.log(sum(1)(2,3))
